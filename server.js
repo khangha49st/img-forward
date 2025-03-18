20e64aefdf18fb571df182003acc87ce476aaddd
Underbase <122473829+khangha49st@users.noreply.github.com>
2025-03-18 05:40:12 +0000

    chore: updated fws config

--- server.js
+++ server.js
@@ -20,8 +20,22 @@ app.use(
       return newPath;
     },
     onProxyReq: (proxyReq) => {
-      // Set custom headers
-      proxyReq.setHeader('Referer', 'https://wanderlog.com/');
+      // Set Referer header
+      const referers = ["https://google.com/", "https://facebook.com/", "https://x.com/", DEFAULT_REFERER];
+      proxyReq.setHeader("Referer", referers[Math.floor(Math.random() * referers.length)]);
+
+      // Random Accept-Language
+      const languages = ["en-US,en;q=0.9", "fr-FR,fr;q=0.9"];
+      proxyReq.setHeader("Accept-Language", languages[Math.floor(Math.random() * languages.length)]);
+      proxyReq.setHeader("Accept-Encoding", "gzip, deflate, br");
+      // Random User-Agent
+      const userAgents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 Chrome/92.0.4515.107 Safari/537.36",
+        "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 Chrome/88.0.4324.181 Mobile Safari/537.36",
+      ];
+      const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+      proxyReq.setHeader("User-Agent", randomUserAgent);
     },
   })
 );
